117e0e14966c0556daf01609b8ad9092321ad9cc
DubigeNagaraju <raj949840@gmail.com>
2024-07-19 22:24:37 +0530

    Add files via upload

---
+++ MySelf.java
@@ -0,0 +1,20 @@
+package anudip;
+
+public class MySelf {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello,I am Nagaraj");
+		System.out.println("I am student");
+		System.out.println("I am from Nizamabad");
+		System.out.println("I have completed my degree");
+		System.out.println("I am pursing MCA in AVNT");
+		System.out.println("My hobbie are playing cricket");
+		System.out.println("I am self motivated");
+		System.out.println("I am a quick learner");
+		System.out.println("My father name is Narendhar");
+		System.out.println("My mother name is Shashikala");
+	
+	}
+
+}
